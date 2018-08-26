404a9d9dd0ade4e9f8976f25b955f71f59a5956c
iamgqr <1824728600@qq.com>
2018-08-26 21:47:46 +0800

    Making callbacks easier
    Just look at those insignificant changes

--- function.autoRenew.js
+++ function.autoRenew.js
@@ -1,28 +1,34 @@
 
-module.exports = {
-    autoRenew:function(creep){
-        var spawn = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => {
-                var range=Game.map.getRoomLinearDistance(structure.room.name,creep.room.name)*50+Memory.constReactTime;
-                return (structure.structureType == STRUCTURE_SPAWN) &&
-                    (!structure.spawning||structure.spawning.remainingTime<=range);
-            }
-        });
-        if(creep.ticksToLive<300||Game.time-creep.memory.needRenew<=creep.body.length*2){
-            if(spawn){
-                if(!creep.pos.inRangeTo(spawn,3)){
-                    creep.moveTo(spawn, {visualizePathStyle: {stroke: '#ffff66'}});
-                }
-                else{
-                    if(spawn.memory.needRenew.time!=Game.time){
-                        if(!creep.pos.inRangeTo(spawn,1))creep.moveTo(spawn, {visualizePathStyle: {stroke: '#ffff66'}});
-                        spawn.renewCreep(creep);
-                    }
-                }
-                spawn.memory.needRenew={time:Game.time,name:creep.name};
-            }
-            if(creep.ticksToLive<300) creep.memory.needRenew=Game.time;
-            return 1;
+module.exports = function(creep){
+    
+    var spawn = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: (structure) => {
+            var range=Game.map.getRoomLinearDistance(structure.room.name,creep.room.name)*50+Memory.constReactTime;
+            return (structure.structureType == STRUCTURE_SPAWN) &&
+                (!structure.spawning||structure.spawning.remainingTime<=range);
         }
+    });
+    if(creep.ticksToLive<300||Game.time-creep.memory.needRenew<=creep.body.length*2){
+        if(spawn){
+            if(!creep.pos.inRangeTo(spawn,3)){
+                creep.moveTo(spawn, {visualizePathStyle: {stroke: '#ffff66'}});
+            }
+            else{
+                if(spawn.memory.needRenew.time!=Game.time){
+                    if(!creep.pos.inRangeTo(spawn,1)) creep.moveTo(spawn, {visualizePathStyle: {stroke: '#ffff66'}});
+                    spawn.renewCreep(creep);
+                    var renewingCreepName = creep.name;
+                    spawn.room.visual.text(
+                        '🔄 ' + renewingCreepName,
+                        spawn.pos.x + 1, 
+                        spawn.pos.y-1, 
+                        {align: 'left', opacity: 0.8});
+                    console.log('Renewing creep '+renewingCreepName);
+                }
+            }
+            spawn.memory.needRenew={time:Game.time,name:creep.name};
+        }
+        if(creep.ticksToLive<300) creep.memory.needRenew=Game.time;
+        return 1;
     }
 };
--- function.findEnemy.js
+++ function.findEnemy.js
@@ -2,14 +2,12 @@
  * MODULE function.findEnemy
  */
 
-module.exports = {
-    findEnemy:function(creep){
-        var pos=creep;
-        if(pos.pos) pos=pos.pos;
-        var target = pos.findClosestByRange(FIND_HOSTILE_CREEPS,{filter: object => Memory.whitelist.indexOf(object.owner.username)==-1});
-        var invader = pos.findClosestByRange(FIND_CREEPS, {filter: { owner: { username: 'Invader' } }});
-        if(invader) target=invader;
-        if(target) Game.notify('Creep '+creep.name+' has been visited by '+target.owner.username+'\'s '+target.name+' at '+Game.time,60);
-        return target;
-    }
+module.exports = function(creep){
+    var pos=creep;
+    if(pos.pos) pos=pos.pos;
+    var target = pos.findClosestByRange(FIND_HOSTILE_CREEPS,{filter: object => Memory.whitelist.indexOf(object.owner.username)==-1});
+    var invader = pos.findClosestByRange(FIND_CREEPS, {filter: { owner: { username: 'Invader' } }});
+    if(invader) target=invader;
+    if(target) Game.notify('Creep '+creep.name+' has been visited by '+target.owner.username+'\'s '+target.name+' at '+Game.time,60);
+    return target;
 };
